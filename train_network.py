fb77a3268fc32aaa83f690b7bb654f9ea4393f59
JMFlin <janne.flinck@yahoo.com>
2018-12-15 10:58:11 +0200

    ran auto-pep8

--- train_network.py
+++ train_network.py
@@ -1,22 +1,35 @@
 # set the matplotlib backend so figures can be saved in the background
+import time
+import os
+import cv2
+import random
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from networks.convnet3.convnet3 import ConvNet3
+from networks.lenet.lenet import LeNet
+from keras.utils import to_categorical
+from keras.preprocessing.image import img_to_array
+from sklearn.model_selection import train_test_split
 import matplotlib
 matplotlib.use("Agg")
- 
+
 # import the necessary packages
 #from keras.preprocessing.image import ImageDataGenerator
 #from keras.optimizers import Adam
-from sklearn.model_selection import train_test_split
-from keras.preprocessing.image import img_to_array
-from keras.utils import to_categorical
-from networks.lenet.lenet import LeNet
-from networks.convnet3.convnet3 import ConvNet3
-import matplotlib.pyplot as plt
-import numpy as np
-import argparse
-import random
-import cv2
-import os
-import time
+
+
+# initialize the number of epochs to train for, initial learning rate,
+# and batch size
+EPOCHS = 35
+INIT_LR = 1e-3
+BS = 32
+IMAGE_WIDTH = 84  # 128 #343
+IMAGE_HEIGTH = 84  # 128 #500
+directory_positive = os.getcwd() + "/images/positive/"
+directory_negative = os.getcwd() + "/images/negative/"
+plot_directory = os.getcwd() + "/networks/lenet/training-plots/"
+model_directory = os.getcwd() + "/networks/lenet/models/"
 
 
 def load_images(directory_positive, directory_negative):
@@ -34,21 +47,21 @@ def load_images(directory_positive, directory_negative):
 
         #label = "santa"
 
-        label = 1 #if label == "santa" else 0
+        label = 1  # if label == "santa" else 0
         labels.append(label)
- 
+
     for filename in os.listdir(directory_negative):
 
         imagePath = directory_negative + filename
 
         image = cv2.imread(imagePath)
-        
+
         image = cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGTH))
         image = img_to_array(image)
         data.append(image)
 
-        #label = "not_santa"#imagePath.split(os.path.sep)[-1]
-        label = 0 #if label == "santa" else 0
+        # label = "not_santa"#imagePath.split(os.path.sep)[-1]
+        label = 0  # if label == "santa" else 0
         labels.append(label)
 
     # scale the raw pixel intensities to the range [0, 1]
@@ -57,29 +70,36 @@ def load_images(directory_positive, directory_negative):
 
     return(data, labels)
 
+
 def plot_training(epochs, history, plot_directory, add):
     # plot the training loss and accuracy
     plt.style.use("ggplot")
     plt.figure()
 
     plt.plot(np.arange(0, epochs), history.history["loss"], label="train_loss")
-    plt.plot(np.arange(0, epochs), history.history["val_loss"], label="val_loss")
+    plt.plot(
+        np.arange(
+            0,
+            epochs),
+        history.history["val_loss"],
+        label="val_loss")
     plt.plot(np.arange(0, epochs), history.history["acc"], label="train_acc")
     plt.plot(np.arange(0, epochs), history.history["val_acc"], label="val_acc")
-    plt.ylim(ymax = 1, ymin = 0)
+    plt.ylim(ymax=1, ymin=0)
     plt.title("Training Loss and Accuracy")
     plt.xlabel("Epoch #")
     plt.ylabel("Loss/Accuracy")
     plt.legend(loc="lower left")
-    plt.savefig(plot_directory + "training-loss-and-accuracy-" + str(time.strftime("%Y%m%d-%H%M%S")) + add +".jpg")
+    plt.savefig(plot_directory + "training-loss-and-accuracy-" +
+                str(time.strftime("%Y%m%d-%H%M%S")) + add + ".jpg")
 
 
 def create_train_test(data, labels):
     # partition the data into training and testing splits using 75% of
     # the data for training and the remaining 25% for testing
-    (trainX, testX, trainY, testY) = train_test_split(data,
-        labels, test_size=0.25, random_state=42)
-     
+    (trainX, testX, trainY, testY) = train_test_split(
+        data, labels, test_size=0.25, random_state=42)
+
     # convert the labels from integers to vectors
     trainY = to_categorical(trainY, num_classes=2)
     testY = to_categorical(testY, num_classes=2)
@@ -88,19 +108,7 @@ def create_train_test(data, labels):
 
 
 if __name__ == '__main__':
-    
-    # initialize the number of epochs to train for, initial learning rate,
-    # and batch size
-    EPOCHS = 35
-    INIT_LR = 1e-3
-    BS = 32
-    IMAGE_WIDTH = 84 #128 #343
-    IMAGE_HEIGTH = 84 #128 #500
-    directory_positive = os.getcwd() + "/images/positive/" 
-    directory_negative = os.getcwd() + "/images/negative/"
-    plot_directory = os.getcwd() + "/networks/lenet/training-plots/"
-    model_directory = os.getcwd() + "/networks/lenet/models/"
-     
+
     # initialize the data and labels
     print("[INFO] loading images...")
     data, labels = load_images(directory_positive, directory_negative)
@@ -110,8 +118,17 @@ if __name__ == '__main__':
 
     # initialize the model
     print("[INFO] building and compiling model...")
-    model = LeNet.build(width=IMAGE_WIDTH, height=IMAGE_HEIGTH, depth=3, classes=2)
-    model = LeNet.compile(model=model, lr=INIT_LR, decay=INIT_LR / EPOCHS, metrics = "accuracy")
+    model = LeNet.build(
+        width=IMAGE_WIDTH,
+        height=IMAGE_HEIGTH,
+        depth=3,
+        classes=2)
+    
+    model = LeNet.compile(
+        model=model,
+        lr=INIT_LR,
+        decay=INIT_LR / EPOCHS,
+        metrics="accuracy")
 
     # train the network
     print("[INFO] training network...")
@@ -119,15 +136,24 @@ if __name__ == '__main__':
 
     ###
     #print(model.evaluate(testX, testY, batch_size=128))
-    ###Scalar test loss
+    # Scalar test loss
 
     print("[INFO] creating validation plot...")
-    plot_training(EPOCHS, history, plot_directory, add = "-" + str(IMAGE_WIDTH) + "-" + str(IMAGE_HEIGTH))
-     
+    plot_training(
+        EPOCHS,
+        history,
+        plot_directory,
+        add="-" +
+        str(IMAGE_WIDTH) +
+        "-" +
+        str(IMAGE_HEIGTH))
+
     # save the model to disk
     print("[INFO] serializing network...")
-    model.save(model_directory + "model-" + str(IMAGE_WIDTH) + "-" + str(IMAGE_HEIGTH) +".h5")
-
-    ## Crowd of people
-    #https://www.google.fi/search?rlz=1C1GGRV_enFI774FI774&biw=1536&bih=723&tbm=isch&sa=1&ei=RkMIXOvcMYuJrwTTjYyYAg&q=crowd+of+people&oq=crowd&gs_l=img.3.2.0i67j0j0i67l4j0j0i67l3.1662.2301..5467...0.0..0.70.265.5....3..1....1..gws-wiz-img.yQzZD4gskXk
-    #
+    model.save(
+        model_directory +
+        "model-" +
+        str(IMAGE_WIDTH) +
+        "-" +
+        str(IMAGE_HEIGTH) +
+        ".h5")
